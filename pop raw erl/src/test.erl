5f2b7a1c3f2be53f280e630f8e17861a392277fc
Johan <Johan@212.25.149.208>
2012-05-02 13:54:39 +0200

    Erlang committed

---
+++ pop raw erl/src/test.erl
@@ -0,0 +1,25 @@
+%% Author: Johan
+%% Created: 2 maj 2012
+%% Description: TODO: Add description to test
+-module(test).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([foo/1]).
+
+%%
+%% API Functions
+%%
+foo(N)->
+	N.
+
+
+%%
+%% Local Functions
+%%
+
