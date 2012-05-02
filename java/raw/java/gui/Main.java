61def45848cde3fa763f564c571ecc490f0a4893
Johan <Johan@212.25.149.208>
2012-05-02 13:30:59 +0200

    First ever commit

---
+++ java/raw/java/gui/Main.java
@@ -0,0 +1,17 @@
+package raw.java.gui;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
