d0c2e1c3ac53a7cf8f8e4fdc2be2c4a11aca6997
theKGS <andreas.lindmark@hutb.se>
2012-05-03 14:59:00 +0200

    wolf/rab v2

---
+++ pop raw/src/raw/java/gui/gfxWolf.java
@@ -0,0 +1,19 @@
+package raw.java.gui;
+
+public class gfxWolf {
+	private int x;
+	private int y;
+	
+	public gfxWolf(int iX, int iY){
+		x = iX;
+		y = iY;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+}
