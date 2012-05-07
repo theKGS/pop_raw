4d4e431022474bece546ab94b8d90f40f730b413
Daniel Gustafsson <r3dsmax@hotmail.com>
2012-05-07 14:56:48 +0200

    2nd draft of Jinterface

--- pop raw/src/raw/java/j_int_java/Gui_send.java
+++ pop raw/src/raw/java/j_int_java/Gui_send.java
@@ -1,9 +1,30 @@
 package raw.java.j_int_java;
 
-public class Gui_send implements Runnable{
+import com.ericsson.otp.erlang.*;
 
+public class Gui_send implements Runnable{
+	private FIFO queue;
+	
+	public Gui_send(FIFO queue) {
+		this.queue = queue;
+	}
 
 	public void run() {
-		// TODO Send methods		
+		while(true) {
+			OtpErlangTuple message = enCode(queue.get());
+		// TODO fetch message from FIFO, encode it and send it.
+		}
+	}
+	
+	private OtpErlangTuple enCode(Message msg) {
+		String sType = msg.getType();
+		int[] values = msg.getValue();
+		int size = values.length;
+		OtpErlangObject[] message = new OtpErlangObject[size+1];
+		message[0] = new OtpErlangAtom(sType);
+		for (int i = 0; i <= size; i++) {
+			message[i+1] = new OtpErlangInt(values[i]);
+		}
+		return new OtpErlangTuple(message);
 	}
 }
