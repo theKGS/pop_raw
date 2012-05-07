6fc39d9c24750659beb1b26814e788e0a039b3d5
Johan <Johan@212.25.145.239>
2012-05-07 14:32:06 +0200

    MapNode and skeleton added

--- pop raw/src/raw/java/map/Map.java
+++ pop raw/src/raw/java/map/Map.java
@@ -1,5 +1,19 @@
 package raw.java.map;
 
+import java.util.ArrayList;
+
 public class Map {
-	
+	ArrayList<ArrayList<MapNode>> mapArray;
+	public Map(int Size, int Seed) {
+		
+	}
+	public void start(){
+		
+	}
+	public void stop(){
+		
+	}
+	public void reset(){
+		
+	}
 }
