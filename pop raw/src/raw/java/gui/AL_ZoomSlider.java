a6a5f3bb1091a0d3ffd039461b28003b03c636ac
theKGS <andreas.lindmark@hutb.se>
2012-05-09 15:00:56 +0200

    Added new slider

---
+++ pop raw/src/raw/java/gui/AL_ZoomSlider.java
@@ -0,0 +1,30 @@
+package raw.java.gui;
+
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import raw.java.map.Map;
+
+/*
+ * A listener for the speed slider.
+ */
+
+public class AL_ZoomSlider implements ChangeListener{
+	MapPanel mPanel;
+	JSlider slider;
+	
+	public AL_ZoomSlider(MapPanel mp, JSlider jsl){
+		mPanel = mp;
+		slider = jsl;
+	}
+	
+	/**
+	 * Called by the JSlider when the value changes
+	 */
+	@Override
+	public void stateChanged(ChangeEvent arg0) {
+		//mp.setSimulationSpeed(slider.getValue());
+		mPanel.setZoom(slider.getValue());
+		System.out.println(slider.getValue()+" ");
+	}
+}
