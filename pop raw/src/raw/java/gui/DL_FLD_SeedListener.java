c5c81d3d395ab002af18953f2cb230b3e1ca0134
theKGS <andreas.lindmark@hutb.se>
2012-05-10 11:11:56 +0200

    DocumentListeners

---
+++ pop raw/src/raw/java/gui/DL_FLD_SeedListener.java
@@ -0,0 +1,25 @@
+package raw.java.gui;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+public class DL_FLD_SeedListener implements DocumentListener {
+
+	@Override
+	public void changedUpdate(DocumentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertUpdate(DocumentEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
