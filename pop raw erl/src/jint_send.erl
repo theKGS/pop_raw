f20113e5daabc767d43a6a769b52ec34c52b0a92
Daniel Gustafsson <r3dsmax@hotmail.com>
2012-05-22 12:20:42 +0200

    rabbitMap and wolfMap added

--- pop raw erl/src/jint_send.erl
+++ pop raw erl/src/jint_send.erl
@@ -15,6 +15,9 @@ send() ->
 	Host = net_adm:localhost(),
 	Address = erlang:list_to_atom(lists:append(Node, Host)),
 	receive
+		%% rabbitMap = 0
+		{rabbitMap, A, B, C} ->
+			{sparta, Address} ! {0, A, B, C};
 		%% rabbitEat = 1
 		{rabbitEat, A, B, C} ->
 			{sparta, Address} ! {1, A, B, C};
@@ -27,7 +30,8 @@ send() ->
 		%% wolfEat = 6
 		{wolfEat, A, B, C} ->
 			{sparta, Address} ! {6, A, B, C};
-		Msg ->
-			{sparta, Address} ! Msg
+		%% wolfMap = 7
+		{wolfMap, A, B, C} ->
+			{sparta, Address} ! {7, A, B, C}
 	end,
 	send().
