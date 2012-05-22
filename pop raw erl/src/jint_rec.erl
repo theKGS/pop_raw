95f02b2e40b5bc82fbc7d57c3ce8309d0f30292f
Daniel Gustafsson <r3dsmax@hotmail.com>
2012-05-22 11:58:00 +0200

    Types changed from string to int

--- pop raw erl/src/jint_rec.erl
+++ pop raw erl/src/jint_rec.erl
@@ -7,16 +7,32 @@ setup(SendPid) ->
 
 server(SendPid) ->
 	receive
-		{newRabbit, _Pid, X, Y} ->
+		%% 4 = newRabbit
+		{4, _Pid, X, Y} ->
 			rabbits:newRabbit({X, Y}, SendPid),
 			server(SendPid);
-		{newWolf, _Pid, _X, _Y} ->
+		%% 8 = newWolf
+		{8, _Pid, _X, _Y} ->
 			%% TODO New wolves
 			server(SendPid);
-		{A,Pid, B} ->
-			Pid ! {A, B},
+		%% 0 = rabbitMap
+		{0,Pid, B} ->
+			Pid ! {rabbitMap, B},
 			server(SendPid);
-		{A, Pid} ->
-			Pid ! A,
+		%% 0 = wolfMap
+		{7,Pid, B} ->
+			Pid ! {wolfMap, B},
+			server(SendPid);
+		%% 9 = yes
+		{9, Pid} ->
+			Pid ! {yes},
+			server(SendPid);
+		%% 10 = no
+		{10, Pid} ->
+			Pid ! {no},
+			server(SendPid);
+		%% 11 = start
+		{11, Pid} ->
+			Pid ! {start},
 			server(SendPid)
 	end.
