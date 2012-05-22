b00b0fcd68f48fc21bb9082a6450d650a4173aa9
theKGS <andreas.lindmark@hutb.se>
2012-05-22 13:52:48 +0200

    More comments.

--- pop raw/src/raw/java/gui/AL_InitButton.java
+++ pop raw/src/raw/java/gui/AL_InitButton.java
@@ -4,23 +4,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import raw.java.map.Map;
 
-/*
- * Listener for the reset button
+/**
+ * Listener for the reset button.
+ * @author andreas
  */
 public class AL_InitButton implements ActionListener{	
 	private MapPanel mPanel;
 	
 	/**
 	 * Constructor. Is passed a reference to a MapPanel.
-	 * @param mp  the MapPanel the check box will be linked to.
+	 * @param mp  the MapPanel the JButton will be linked to.
 	 */
 	public AL_InitButton(MapPanel mp) {
 		mPanel = mp;
 	}
 
 	/**
-	 * Called by the init button
-	 * Resets simulation with new seed.
+	 * Called by the JButton button.
+	 * Resets the simulation with new seed and new size.
 	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
--- pop raw/src/raw/java/gui/AL_StopButton.java
+++ pop raw/src/raw/java/gui/AL_StopButton.java
@@ -10,12 +10,17 @@ import raw.java.map.Map;
 public class AL_StopButton implements ActionListener{
 	private MapPanel mPanel;
 	
+	/**
+	 * Constructor. Is passed a reference to a MapPanel.
+	 * @param mp  the MapPanel the JButton will be linked to.
+	 */
 	public AL_StopButton(MapPanel mp) {
 		mPanel = mp;
 	}
 
 	/**
-	 * Called by the stop button
+	 * Called by the JButton button.
+	 * Stops the simulation if it has been paused.
 	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
